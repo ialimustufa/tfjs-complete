f5e4edbdf52f14af9fda309823e07cdf467cc055
Glitch (hello-webpage) <none>
2019-08-10 02:58:20 +0000

    💈🚙 Checkpoint ./index.html:6022716/3322 ./index.js:6022716/3291

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let net;
 const webcamElement = document.getElementById('webcam');
-const classifier = knnClassifier.create();
-
 async function app() {
   console.log('Loading mobilenet..');
 
@@ -9,58 +7,10 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  await setupWebcam();
-
-  // Reads an image from the webcam and associates it with a specific class
-  // index.
-  const addExample = classId => {
-    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-    // to the KNN classifier.
-    const activation = net.infer(webcamElement, 'conv_preds');
-
-    // Pass the intermediate activation to the classifier.
-    classifier.addExample(activation, classId);
-  };
-
-  // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
-
-  while (true) {
-    if (classifier.getNumClasses() > 0) {
-      // Get the activation from mobilenet from the webcam.
-      const activation = net.infer(webcamElement, 'conv_preds');
-      // Get the most likely class and confidences from the classifier module.
-      const result = await classifier.predictClass(activation);
-
-      const classes = ['A', 'B', 'C'];
-      document.getElementById('console').innerText = `
-        prediction: ${classes[result.classIndex]}\n
-        probability: ${result.confidences[result.classIndex]}
-      `;
-    }
-
-    await tf.nextFrame();
-  }
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
 }
 
-async function setupWebcam() {
-  return new Promise((resolve, reject) => {
-    const navigatorAny = navigator;
-    navigator.getUserMedia = navigator.getUserMedia ||
-        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-        navigatorAny.msGetUserMedia;
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true},
-        stream => {
-          webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-        },
-        error => reject());
-    } else {
-      reject();
-    }
-  });
-}
 app();
